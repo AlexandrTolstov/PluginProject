66d2a5199e9076553ec520f7a11e86a3241e1067
AlexandrTolstov <alextav_85@mail.ru>
2019-01-16 22:30:52 +0600

    1

--- PluginProject/MainWindow.xaml.cs
+++ PluginProject/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,12 +23,14 @@ namespace PluginProject
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly string pluginPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "Plugins");
+        private readonly string pluginPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "Plugins"); //Создаем строку из текущей директории и имяни папки (Plugins)
         private List<IPlugin> plugins = new List<IPlugin>();
         public MainWindow()
         {
             InitializeComponent();
 
+            //обновляем список плагинов
+            RefreshPlugins();
         }
 
         private void BtnRefineText_Click(object sender, RoutedEventArgs e)
@@ -36,8 +39,36 @@ namespace PluginProject
 
             //TODO: пройти по списку плагинов и дать каждому
             //обработать текст, сохрагяя результат в result
+            foreach (var plugin in plugins)
+                result = plugin.Activate(result);
 
             txtPhrase.Text = result;
+        }
+        private void RefreshPlugins()
+        {
+            plugins.Clear();
+
+            DirectoryInfo pluginDirectory = new DirectoryInfo(pluginPath);
+
+            if (!pluginDirectory.Exists)
+                pluginDirectory.Create(); //Создать  директорию по заданному пути
+
+            string[] pluginFiles = Directory.GetFiles(pluginPath, "*.dll");
+            foreach (var file in pluginFiles)
+            {
+                //Загружаем сборку
+                Assembly asm = Assembly.LoadFrom(file);
+                //ищем типы, имплементирующие наш интерфейс IPlugin,
+                //чтобы не захватить лишнего
+                var types = asm.GetTypes().Where(t => t.GetInterfaces().
+                                            Where(i => i.FullName == typeof(IPlugin).FullName).Any());
+                //заполняем экземплярами полученных типов коллекцию плагинов
+                foreach (var type in types)
+                {
+                    var plugin = asm.CreateInstance(type.FullName) as IPlugin;
+                    plugins.Add(plugin);
+                }
+            }
         }
     }
 }
---
+++ TextToLower/TextToLowerClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginInterface;
+
+namespace TextToLower
+{
+    public class TextToLowerClass
+    {
+        public class TextToLowerPlugin : IPlugin
+        {
+            public string Activate(string input)
+            {
+                //переводим прописные буквы в строчные    
+                return input.ToLower();
+            }
+        }
+    }
+}
